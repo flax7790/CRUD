82996fb1b724f6aeb3e576347958a098e081278a
yevgenii <flax77_90@mail.ru>
2017-04-30 12:25:30 +0300

    add indexAction

--- index.php
+++ index.php
@@ -15,9 +15,9 @@ echo "Hello, World!";
 </form>
 
 <?php
+$pdo_conn = new PDO("mysql:host=127.0.0.1; dbname=CRUD", 'root', 'ryabus');
 
 if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
-    $pdo_conn = new PDO("mysql:host=127.0.0.1; dbname=CRUD", 'root', 'ryabus');
     $sql = 'INSERT INTO article (name, description, created_at) VALUE (:name, :description, :created_at)';
     $statement = $pdo_conn->prepare($sql);
     $statement->bindValue(':name', $_POST['name']);
@@ -25,5 +25,17 @@ if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['creat
     $statement->bindValue(':created_at', $_POST['created_at']);
 
     var_dump($statement->execute());
-    var_dump($statement->errorInfo());
+    //var_dump($statement->errorInfo());
+}
+
+$sql = 'SELECT * FROM article ORDER BY created_at';
+
+$statement = $pdo_conn->prepare($sql);
+$statement->execute();
+$articles = $statement->fetchAll();
+foreach ($articles as $row) {
+    echo '<br>';
+    print $row['name'] . "\t";
+    print $row['description'] . "\t";
+    print $row['created_at'] . "\n";
 }
