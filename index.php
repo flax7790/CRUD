107b61e895564e4beb27d859f059c1ea186b8d3e
yevgenii <flax77_90@mail.ru>
2017-04-30 15:24:43 +0300

    added MVC

--- index.php
+++ index.php
@@ -1,41 +1,12 @@
 <?php
 
-echo "Hello, World!";
-
-?>
-
-<form method="post" action="index.php">
-    <input type="text" placeholder="name" name="name">
-    <br>
-    <input type="text" placeholder="description" name="description">
-    <br>
-    <input type="text" placeholder="created_at" name="created_at">
-    <br>
-    <input type="submit">
-</form>
-
-<?php
-$pdo_conn = new PDO("mysql:host=127.0.0.1; dbname=CRUD", 'root', 'ryabus');
+require_once('model/model.php');
 
 if (isset($_POST['name']) && isset($_POST['description']) && isset($_POST['created_at'])) {
-    $sql = 'INSERT INTO article (name, description, created_at) VALUE (:name, :description, :created_at)';
-    $statement = $pdo_conn->prepare($sql);
-    $statement->bindValue(':name', $_POST['name']);
-    $statement->bindValue(':description', $_POST['description']);
-    $statement->bindValue(':created_at', $_POST['created_at']);
 
-    var_dump($statement->execute());
-    //var_dump($statement->errorInfo());
+    $result = insert($_POST['name'], $_POST['description'], $_POST['created_at']);
+    var_dump($result);
+
 }
 
-$sql = 'SELECT * FROM article ORDER BY created_at';
-
-$statement = $pdo_conn->prepare($sql);
-$statement->execute();
-$articles = $statement->fetchAll();
-foreach ($articles as $row) {
-    echo '<br>';
-    print $row['name'] . "\t";
-    print $row['description'] . "\t";
-    print $row['created_at'] . "\n";
-}
+require_once ('view/indexTemplate.php');
---
+++ model/model.php
@@ -0,0 +1,20 @@
+<?php
+
+function getConnection()
+{
+    $pdo_conn = new PDO("mysql:host=127.0.0.1; dbname=CRUD", 'root', 'ryabus');
+
+    return $pdo_conn;
+}
+
+function insert($name, $description, $created_at)
+{
+    $pdo_conn = getConnection();
+    $sql = 'INSERT INTO article (name, description, created_at) VALUE (:name, :description, :created_at)';
+    $statement = $pdo_conn->prepare($sql);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':description', $description);
+    $statement->bindValue(':created_at', $created_at);
+
+    return $statement->execute();
+}
